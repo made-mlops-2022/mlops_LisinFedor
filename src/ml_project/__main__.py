4bbf0b5805b4b4f00a5f2ecd5dee9f25a792c1f9
Theo <theo.lisin@gmail.com>
2022-11-06 20:25:27 +0300

    add mlflow arguments

--- src/ml_project/__main__.py
+++ src/ml_project/__main__.py
@@ -6,9 +6,12 @@ from ml_project.predict import load_and_predict
 
 def parse_args():
     parser = argparse.ArgumentParser("MLOps HW1")
+
     main_group = parser.add_mutually_exclusive_group(required=True)
     main_group.add_argument(
-        "--train", action="store_true", help="run train pipline using config.yml file."
+        "--train",
+        action="store_true",
+        help="run train pipline using config.yml file.",
     )
 
     main_group.add_argument(
@@ -37,7 +40,30 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--input", help="path to file for prediction; required argument for prediction",
+        "--input",
+        help="path to file for prediction; required argument for prediction",
+    )
+
+    parser.add_argument(
+        "--modelname",
+        help="this name will be used for saving model, kwargs config and mlflow",
+        action="store",
+    )
+
+    mlflow_group = parser.add_mutually_exclusive_group()
+
+    mlflow_group.add_argument(
+        "--mlflow",
+        help="store model and artifacts into mlflow; "
+        "if not provided use_mlflow parameter from config will be used",
+        action="store_true",
+    )
+
+    mlflow_group.add_argument(
+        "--no-mlflow",
+        help="prevent saving experiment to mlflow; "
+        "if not provided use_mlflow parameter from config will be used",
+        action="store_true",
     )
 
     return parser.parse_args()
@@ -46,11 +72,13 @@ def parse_args():
 def create_prediction_kwargs(parsed_args) -> Dict[str, str]:
     if parsed_args.input is None:
         raise argparse.ArgumentError(
-            parsed_args.input, "--input is required for prediction.",
+            parsed_args.input,
+            "--input is required for prediction.",
         )
     if parsed_args.outfile is None:
         raise argparse.ArgumentError(
-            parsed_args.outfile, "--outfile is required for prediction.",
+            parsed_args.outfile,
+            "--outfile is required for prediction.",
         )
 
     kwargs = {}
@@ -65,15 +93,30 @@ def create_prediction_kwargs(parsed_args) -> Dict[str, str]:
         kwargs["data_path"] = parsed_args.input
     else:
         raise argparse.ArgumentError(
-            parsed_args.input, "--input path is required for prediction",
+            parsed_args.input,
+            "--input path is required for prediction",
         )
 
     if parsed_args.outfile:
         kwargs["out_csv_file_path"] = parsed_args.outfile
     else:
         raise argparse.ArgumentError(
-            parsed_args.outfile, "--outfile path is required for prediction",
+            parsed_args.outfile,
+            "--outfile path is required for prediction",
         )
+
+    return kwargs
+
+
+def create_train_kwargs(parsed_args):
+    kwargs = {"use_mlflow": None}
+    if parsed_args.mlflow:
+        kwargs["use_mlflow"] = True
+    elif parsed_args.no_mlflow:
+        kwargs["use_mlflow"] = False
+
+    if parsed_args.modelname:
+        kwargs["save_as"] = parsed_args.modelname
 
     return kwargs
 
@@ -84,7 +127,8 @@ def main():
         kwargs = create_prediction_kwargs(parsed_args)
         load_and_predict(**kwargs)
     else:
-        train_pipeline()
+        kwargs = create_train_kwargs(parsed_args)
+        train_pipeline(**kwargs)
 
 
 if __name__ == "__main__":
