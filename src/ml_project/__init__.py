582ba828eb513ec99042caa5cdac1dc976befc4e
Theo <theo.lisin@gmail.com>
2022-11-23 19:59:45 +0300

    ad  logger params

--- src/ml_project/__init__.py
+++ src/ml_project/__init__.py
@@ -4,7 +4,7 @@ import sys
 logger = logging.getLogger("ml_project")
 str_handler = logging.StreamHandler(sys.stdout)
 fmt = logging.Formatter(
-    "%(asctime)s\t%(levelname)s\t[logger: %(name)s]: %(message)s",
+    "%(asctime)s\t%(levelname)s\t[%(name)s]: %(message)s",
 )
 logger.setLevel(logging.INFO)
 str_handler.setFormatter(fmt)
